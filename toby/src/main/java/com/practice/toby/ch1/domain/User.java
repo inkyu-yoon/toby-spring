4cb40709b7df5126e5edc6b0042c0640e710e5f1
inkyu-yoon <tryingmybest24h@gmail.com>
2023-05-10 19:42:10 +0900

    [feat] : 초난감 USER DAO

---
+++ toby/src/main/java/com/practice/toby/ch1/domain/User.java
@@ -0,0 +1,13 @@
+package com.practice.toby.ch1.domain;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class User {
+    String id;
+    String name;
+    String password;
+
+}

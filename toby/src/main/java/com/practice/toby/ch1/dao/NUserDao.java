2af95947b29ce1851d0e109dacc8ca65f654ea2e
inkyu-yoon <tryingmybest24h@gmail.com>
2023-05-15 20:50:27 +0900

    [feat] : 상속을 통한 확장
    - DB connection 연결하는 메서드를 추상메서드로 선언함으로서, 구현체에 따라 각자 필요한 DB 커넥션을 설정하도록 함

---
+++ toby/src/main/java/com/practice/toby/ch1/dao/NUserDao.java
@@ -0,0 +1,17 @@
+package com.practice.toby.ch1.dao;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class NUserDao extends UserDao{
+    @Override
+    public Connection getConnection() throws ClassNotFoundException, SQLException  {
+        // 1. DB 연결을 위한 커넥션
+        Class.forName("com.mysql.cj.jdbc.Driver");
+        Connection c = DriverManager.getConnection("jdbc:mysql://localhost:3306/toby-db", "root", "12341234");
+        return c;
+    }
+
+
+}
--- toby/src/main/java/com/practice/toby/ch1/dao/UserDao.java
+++ toby/src/main/java/com/practice/toby/ch1/dao/UserDao.java
@@ -4,29 +4,13 @@ import com.practice.toby.ch1.domain.User;
 
 import java.sql.*;
 
-public class UserDao {
-
-    public static void main(String[] args) throws SQLException, ClassNotFoundException {
-        UserDao dao = new UserDao();
-        User user = new User();
-        user.setId("inkyu");
-        user.setName("윤인규");
-        user.setPassword("password");
-
-        dao.add(user);
-
-        System.out.println(user.getId() + " 등록 성공");
-
-        User user2 = dao.get(user.getId());
-        System.out.println(user2.getName());
-        System.out.println(user2.getPassword());
-        System.out.println(user2.getId()+" 조회 성공");
-    }
+public abstract class UserDao {
 
     public void add(User user) throws ClassNotFoundException, SQLException {
-        Class.forName("com.mysql.cj.jdbc.Driver");
-        Connection c = DriverManager.getConnection("jdbc:mysql://localhost:3306/toby-db", "root", "12341234");
 
+        Connection c = getConnection();
+
+        // 2. Statement를 만들고 실행
         PreparedStatement ps = c.prepareStatement(
                 "INSERT INTO USERS(id,name,password) values(?,?,?)"
         );
@@ -37,13 +21,13 @@ public class UserDao {
 
         ps.executeUpdate();
 
+        // 3. 작업이 끝난 뒤, 리소스 반환
         ps.close();
         c.close();
     }
 
     public User get(String id) throws ClassNotFoundException, SQLException {
-        Class.forName("com.mysql.cj.jdbc.Driver");
-        Connection c = DriverManager.getConnection("jdbc:mysql://localhost:3306/toby-db", "root", "12341234");
+        Connection c = getConnection();
 
         PreparedStatement ps = c.prepareStatement(
                     "SELECT * FROM USERS WHERE id = ?"
@@ -64,4 +48,6 @@ public class UserDao {
 
         return user;
     }
+
+    public abstract Connection getConnection() throws ClassNotFoundException, SQLException;
 }

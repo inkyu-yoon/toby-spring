1d103de1e6a7520e9e44eedebeed43e06d56c701
inkyu-yoon <tryingmybest24h@gmail.com>
2023-05-15 21:57:18 +0900

    [feat] : spring 방식으로 사용
    - @Configuration과 @Bean 어노테이션을 통해 Application Context(Bean Factory) 사용

--- toby/src/main/java/com/practice/toby/ch1/dao/UserDaoTest.java
+++ toby/src/main/java/com/practice/toby/ch1/dao/UserDaoTest.java
@@ -1,9 +1,12 @@
 package com.practice.toby.ch1.dao;
 
 import com.practice.toby.ch1.factory.DaoFactory;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
 public class UserDaoTest {
     public static void main(String[] args) {
-        UserDao userDao = new DaoFactory().userDao();
+        ApplicationContext context = new AnnotationConfigApplicationContext(DaoFactory.class);
+        UserDao dao = context.getBean("userDao", UserDao.class);
     }
 }
--- toby/src/main/java/com/practice/toby/ch1/factory/DaoFactory.java
+++ toby/src/main/java/com/practice/toby/ch1/factory/DaoFactory.java
@@ -3,12 +3,17 @@ package com.practice.toby.ch1.factory;
 import com.practice.toby.ch1.dao.UserDao;
 import com.practice.toby.ch1.db.ConnectionMaker;
 import com.practice.toby.ch1.db.SimpleConnectionMaker;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 
+@Configuration
 public class DaoFactory {
+    @Bean
     public UserDao userDao() {
         return new UserDao(getConnectionMaker());
     }
 
+    @Bean
     public ConnectionMaker getConnectionMaker() {
         return new SimpleConnectionMaker();
     }

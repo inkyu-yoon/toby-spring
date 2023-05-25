4d9ba46a0134eeb0b3937817e1f476a51d1c7ef6
inkyu-yoon <tryingmybest24h@gmail.com>
2023-05-25 21:53:42 +0900

    [feat] : @BeforeEach 를 활용한 테스트 코드 구조 개선

--- src/test/java/com/practice/toby/ch1/dao/UserDaoTest.java
+++ src/test/java/com/practice/toby/ch1/dao/UserDaoTest.java
@@ -3,24 +3,31 @@ package com.practice.toby.ch1.dao;
 import com.practice.toby.ch1.domain.User;
 import com.practice.toby.ch1.factory.DaoFactory;
 import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 import org.springframework.dao.EmptyResultDataAccessException;
-import org.springframework.test.annotation.Rollback;
 
 import java.sql.SQLException;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 
 class UserDaoTest {
+
+    private UserDao dao;
+
+    @BeforeEach
+    public void setUp() {
+        ApplicationContext context = new AnnotationConfigApplicationContext(DaoFactory.class);
+        dao = context.getBean("userDao", UserDao.class);
+    }
 
     @Test
     @DisplayName("add와 get 테스트 코드")
     public void addAndGet() throws SQLException, ClassNotFoundException {
-        ApplicationContext context = new AnnotationConfigApplicationContext(DaoFactory.class);
-        UserDao dao = context.getBean("userDao", UserDao.class);
+
         dao.deleteAll();
         Assertions.assertThat(dao.getCount()).isEqualTo(0);
 
@@ -38,20 +45,18 @@ class UserDaoTest {
     @Test
     @DisplayName("getCount 테스트 코드")
     public void getCount() throws SQLException {
-        ApplicationContext context = new AnnotationConfigApplicationContext(DaoFactory.class);
-        UserDao dao = context.getBean("userDao", UserDao.class);
         dao.deleteAll();
 
         Assertions.assertThat(dao.getCount()).isEqualTo(0);
 
-        dao.add(new User("id1","name","p"));
+        dao.add(new User("id1", "name", "p"));
         Assertions.assertThat(dao.getCount()).isEqualTo(1);
 
-        dao.add(new User("id2","name","p"));
+        dao.add(new User("id2", "name", "p"));
         Assertions.assertThat(dao.getCount()).isEqualTo(2);
 
 
-        dao.add(new User("id3","name","p"));
+        dao.add(new User("id3", "name", "p"));
         Assertions.assertThat(dao.getCount()).isEqualTo(3);
 
     }
@@ -59,9 +64,7 @@ class UserDaoTest {
     @Test
     @DisplayName("get 메서드 실행 시, 존재하지 않는 경우 예외 발생")
     public void getError() {
-        ApplicationContext context = new AnnotationConfigApplicationContext(DaoFactory.class);
-        UserDao dao = context.getBean("userDao", UserDao.class);
-        assertThrows(EmptyResultDataAccessException.class, () -> dao.get("random"));
 
+        assertThrows(EmptyResultDataAccessException.class, () -> dao.get("random"));
     }
 }

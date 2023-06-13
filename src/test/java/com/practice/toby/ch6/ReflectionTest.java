5f9e8db94292fa22ed491529d645d15c8a4ce0cd
inkyu-yoon <tryingmybest24h@gmail.com>
2023-06-14 07:23:05 +0900

    [test] : reflection api 사용해보기

---
+++ src/test/java/com/practice/toby/ch6/ReflectionTest.java
@@ -0,0 +1,25 @@
+package com.practice.toby.ch6;
+
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
+
+import static org.assertj.core.api.Assertions.*;
+
+public class ReflectionTest {
+
+    @Test
+    public void invokeMethod() throws NoSuchMethodException, InvocationTargetException, IllegalAccessException {
+        String name = "Spring";
+
+        assertThat(name.length()).isEqualTo(6);
+
+        Method lengthMethod = String.class.getMethod("length");
+
+        assertThat((Integer) lengthMethod.invoke(name)).isEqualTo(6);
+
+    }
+
+}
